e3e73b356d89fdb0d47b5f4c3494ca0aad0dfe95
Dakota Dunn <dakotadunn84@gmail.com>
2021-11-02 16:34:29 -0700

    Add React Component Menu and 2nd Song
    Add React Component Menu and 2nd Song

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from "./monkey.png";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home, Song1, Song2, Contact } from "./components";
 import ReactPlayer from "react-player";
 import {
   Navbar,
@@ -11,8 +12,6 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavItem,
-  NavLink
 } from "reactstrap";
 
 function App() {
@@ -20,31 +19,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
-          <Navbar color="dark" dark expand="md" light>
-            <NavbarBrand href="/">Learn By Song</NavbarBrand>
-            <NavbarToggler
-              className="me-2"
-              onClick={function noRefCheck() {}}
-            />
-            <Collapse navbar>
-              <Nav className="me-auto" navbar>
-                <UncontrolledDropdown inNavbar nav>
-                  <DropdownToggle caret nav>
-                    Songs
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    <DropdownItem>Song 1</DropdownItem>
-                    <DropdownItem>Song 2</DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </Nav>
-            </Collapse>
-          </Navbar>
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Learn Mandarin from the lyrics of popular songs at karaoke.</p>
-        <div>
-          <ReactPlayer url="https://www.youtube.com/watch?v=IFTZtuXKzFs" />
+          <Router>
+            <Navigation />
+            <Switch>
+            <Route path="/" exact component={() => <Home />} />
+              <Route path="/Eloping_to_the_moon" exact component={() => <Song1 />} />
+              <Route path="/I_want_your_love" exact component={() => <Song2 />} />
+            </Switch>
+          </Router>
         </div>
       </header>
     </div>
---
+++ my-app/src/components/Eloping to the Moon.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+function Song2() {
+  return (
+    <div className="私奔到月球 Eloping to the Moon">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div>
+            <ReactPlayer url="https://www.youtube.com/watch?v=IFTZtuXKzFs" />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">私奔到月球 Eloping to the Moon</h1>
+            <p>其实妳 是个心狠又手辣 的小偷</p>
+            <p>Actually, you are a ruthless thief</p>
+            <p>我的心 我的呼吸和名字 都偷走</p>
+            <p>Stealing my heart, breath and name away</p>
+            <p>你才是 绑架我的凶手</p>
+            <p>But you were the one who kidnapped me</p>
+            <p>机车后座的我 吹着风 逃离了平庸</p>
+            <p>
+              On the motorcycle backseat, in the gentle wind, escaping from the
+              ordinary
+            </p>
+            <p>这星球 天天有五十亿人 在错过</p>
+            <p>There are fifty million people missing out everyday</p>
+            <p>多幸运 有妳一起看星星 在争宠</p>
+            <p>How lucky I am to have you stargazing
+              with me</p>
+            <p>这一刻 不再问为什么</p>
+            <p>Don’t ask why in this moment</p>
+            <p>不再去猜测人和人 心和心 有什么不同</p>
+            <p>Don’t second guess differences
+              between people and their hearts</p>
+            <p>一二三 牵着手</p>
+            <p>One two three,
+              holding hands</p>
+            <p>四五六 抬起头</p>
+            <p>Four five six, raising heads</p>
+            <p>七八九
+              我们私奔到月球</p>
+            <p>Seven eight nine, let’s elope to the moon</p>
+            <p>让双脚
+              去腾空</p>
+            <p>Let our feet jump into the air</p>
+            <p>让我们 去感受</p>
+            <p>Let us feel
+              this moment</p>
+            <p>那无忧的真空</p>
+            <p>The carefree vacuum</p>
+            <p>那月色纯真的感动</p>
+            <p>The
+              bliss of pure moonlight</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Song2;
